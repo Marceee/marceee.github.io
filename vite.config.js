1080b1f10d9048616975b554b5944f7e5e114477
Marceee <marcelinusande@gmail.com>
2025-10-07 17:46:38 +0300

    add  deployment scripts

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['@rollup/rollup-linux-x64-gnu']
+  //   }
+  // }
 })
