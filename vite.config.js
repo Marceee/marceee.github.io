8fc0bf2b9656bb4e948eb301c2e2e456d71914fe
Marceee <marcelinusande@gmail.com>
2025-10-07 17:57:15 +0300

    add repo base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ms-portfolio/',
   // build: {
   //   rollupOptions: {
   //     external: ['@rollup/rollup-linux-x64-gnu']
