f13d8e1cf335bdc323c004008d0373374e7f69c0
Marceee <marcelinusande@gmail.com>
2025-10-07 18:10:14 +0300

    fix repo base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ms-portfolio/',
+  base: '/',
   // build: {
   //   rollupOptions: {
   //     external: ['@rollup/rollup-linux-x64-gnu']
