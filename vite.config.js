bbdbacff42ecf6e57b546ea5d8087f260eb2f160
Marceee <marcelinusande@gmail.com>
2025-10-09 22:12:18 +0300

    corect repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mcportfolio/',
+  base: '/marceee.github.io',
   // build: {
   //   rollupOptions: {
   //     external: ['@rollup/rollup-linux-x64-gnu']
